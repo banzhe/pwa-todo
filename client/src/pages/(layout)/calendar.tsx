3aa3f1ac9ee44bd604020e24902eb9c968de437f
banzhe <banzhe5@outlook.com>
2025-07-27 16:54:17 +0800

    feat: add big calendar dependency

--- client/src/pages/(layout)/calendar.tsx
+++ client/src/pages/(layout)/calendar.tsx
@@ -1,3 +1,15 @@
+import { useState } from 'react'
+import { ClientContainer } from '@/calendar/components/client-container'
+import { CalendarProvider } from '@/calendar/contexts/calendar-context'
+import type { TCalendarView } from '@/calendar/types'
+
 export default function Calendar() {
-  return <div>Calendar</div>
+  const [view, setView] = useState<TCalendarView>('month')
+  return (
+    <div>
+      <CalendarProvider users={[]} events={[]}>
+        <ClientContainer view={view} onViewChange={setView}></ClientContainer>
+      </CalendarProvider>
+    </div>
+  )
 }
